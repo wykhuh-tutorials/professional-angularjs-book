e705e1234bea59fb01a79e66f5d73cc76b31dedd
Wai-Yin Kwan <waiyin@gophilosophie.com>
2015-10-16 23:01:41 -0700

    3. generate dashboard and watchlist route, views, controllers

---
+++ app/scripts/controllers/watchlist.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('stockDogApp')
+  .controller('WatchlistCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

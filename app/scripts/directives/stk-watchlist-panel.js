b9b59ce10aa2df63035e2d8bfa70f8414eaa82f1
Wai-Yin Kwan <waiyin@gophilosophie.com>
2015-10-16 21:39:00 -0700

    add watchlist directive

---
+++ app/scripts/directives/stk-watchlist-panel.js
@@ -0,0 +1,51 @@
+'use strict';
+
+angular.module('stockDogApp')
+  // register directive and inject dependenices
+  .directive('stkWatchlistPanel', function ($location, $modal, WatchlistService) {
+    // returns an object containing configuration options and a link()
+    return {
+      templateUrl: 'views/templates/watchlist-panel.html',
+      restrict: 'E',
+      // isolating directives scope so that anything attached to the scope
+      // is available only within the context of this directive.
+      scope: {},
+      // directive’s scope variables are initialized
+      link: function ($scope) {
+        // initialize variables
+
+        // watchlist is bound to the form inputs
+        $scope.watchlist = {};
+
+        var addListModal = $modal({
+          scope: $scope,
+          template: 'views/templates/addlist-modal.html',
+          show: false
+        });
+
+        // bind model from service to this directive $scope.
+        // grab all lists from local storage and send them the the view.
+        // called once on page load.
+        $scope.watchlists = WatchlistService.query();
+
+        // display addlist modal
+        $scope.showModal = function () {
+          addListModal.$promise.then(addListModal.show);
+        };
+
+        // create new list from fields in modal
+        $scope.createList = function () {
+          WatchlistService.save($scope.watchlist);
+          addListModal.hide();
+          // clear form
+          $scope.watchlist = {};
+        };
+
+        // delete desired lsit and redirect to home
+        $scope.deleteList = function (list) {
+          WatchlistService.remove(list);
+          $location.path('/');
+        };
+      }
+    };
+  });
--- app/scripts/services/watchlist-service.js
+++ app/scripts/services/watchlist-service.js
@@ -28,8 +28,10 @@ angular.module('stockDogApp')
     // Return all watchlists or find by given ID
     this.query = function (listId) {
       if (listId) {
+        console.log('return one watchlists');
         return findById(listId);
       } else {
+        console.log('return all watchlists');
         return Model.watchlists;
       }
     };
@@ -37,16 +39,24 @@ angular.module('stockDogApp')
     // Save a new watchlist to watchlists model
     this.save = function (watchlist) {
       watchlist.id = Model.nextId++;
+      console.log('before save', Model.watchlists);
+      // add new list to in-memory Model.watchlist.
       Model.watchlists.push(watchlist);
+      // save new list to local storage
       saveModel();
+      console.log('after save', Model.watchlists);
     };
 
     // Remove given watchlist from watchlists model
     this.remove = function (watchlist) {
+      console.log('before delete', Model.watchlists);
+      // remove list from in-memory Model.watchlist.
       _.remove(Model.watchlists, function (list) {
         return list.id === watchlist.id;
       });
+      // save new list to local storage
       saveModel();
+      console.log('after delete', Model.watchlists);
     };
 
     // Initialize Model for this singleton service
